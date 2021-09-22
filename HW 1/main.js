dc16cc1a8d381b7184292015aa6a3b4bcdf5bbc4
олег <gorodeckiy88@gmail.com>
2021-09-22 16:18:59 +0300

    added js

--- HW 1/main.js
+++ HW 1/main.js
@@ -6,17 +6,22 @@ let vegetable = 90.2345;
 document.write("Максимальна ціна:"+Math.max(meat,fruit,vegetable)+";"+"<br>");
 document.write("Мінімальна ціна :"+Math.min(meat,fruit,vegetable)+";"+"<br>");
 let  total = (meat+fruit+vegetable);
-document.write("Загальна сума товарів :"+total+";"+"<br>");
-document.write("Округлення в меншу сторону :"+ Math.floor(total)+";"+"<br>");
-document.write("Округлення до сотень :"+ Math.ceil((total) / 100*100)+";"+"<br>");
-document.write("Булеве значення (округлена в меншу сторону) " + (!!Math.floor((total)%2))+";"+"<br>");
+document.write("Загальна сума товарів :"+total+";"+"<br>")
+
+let priseFloor = (Math.floor(meat) + Math.floor(fruit) + Math.floor(vegetable));
+document.write ("Округлення в меншу сторону :"+ priseFloor +";"+"<br>");
+
+
+document.write("Cуму товарів округлену до сотень :" + (Math.round(priseFloor / 100) * 100)+";"+"<br>")
+
+document.write("Булеве значення (округлена в меншу сторону) :" + (!!Math.floor((total)%2))+";"+"<br>");
 document.write("Cума решти, при оплаті всіх товарів , якщо клієнт платить 500 грн :"+ (500-total)+";"+"<br>");
 
 document.write("Cереднє значення цін, округлене до другого знаку після коми :" + ((total/3)*100/100).toFixed(2) +";"+"<br>");
 
 
-let random = Math.round(Math.random()*100);
- document.write("Випадкове число :"+Math.round(Math.random()*100)+";"+"<br>");
+let random = Math.round (Math.random()*100);
+ document.write("Випадкове число :"+random+";"+"<br>");
  document.write("Сума до Оплати зі Знижкою :"+((100-random)*total/100).toFixed(2) +";"+"<br>")
  let selfPrise = (total/2).toFixed(2);
  document.write("Самостійна ціна :"+ (total/2).toFixed(2) +";"+"<br>");
