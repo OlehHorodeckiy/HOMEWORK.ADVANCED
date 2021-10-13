c360ecac589397543824dcb77dce1a51be6b5c5e
олег <gorodeckiy88@gmail.com>
2021-10-13 19:32:54 +0300

    added start HW

---
+++ HW6/main.js
@@ -0,0 +1,44 @@
+const students = [
+  {
+    name: 'Tanya',
+    course: 3,
+    subjects: {
+      math: [4, 4, 3, 4],
+      algorithms: [3, 3, 3, 4, 4, 4],
+      data_science: [5, 5, 3, 4],
+    },
+  },
+  {
+    name: 'Victor',
+    course: 4,
+    subjects: {
+      physics: [5, 5, 5, 3],
+      economics: [2, 3, 3, 3, 3, 5],
+      geometry: [5, 5, 2, 3, 5],
+    },
+  },
+  {
+    name: 'Anton',
+    course: 2,
+    subjects: {
+      statistics: [4, 5, 5, 5, 5, 3, 4, 3, 4, 5],
+      english: [5, 3],
+      cosmology: [5, 5, 5, 5],
+    },
+  },
+];
+
+// Задача №1 юСтворіть функцію getSubjects(students[0] --> ["Math", "Algorithms", "Data science"] - яка повертає список предметів для конкретного студента. Зверніть увагу – назву предмету необхідно повертати з великої літери, а _ – замінити на пробіл
+
+const getSubjects = (subject) => {
+    let subjects = [];
+    for (let key in subject) {
+      const subjectn = (
+        key.substring(0, 1).toUpperCase() + key.substring(1).toLowerCase()
+      ).replace("_", " ");
+      subjects.push(subjectn);
+    }
+    return subjects;
+  };
+  
+  console.log(getSubjects(students[0].subjects));
