b5a577c2c32b8d5c22d6be4e9f3310d5e1ec5cd9
олег <gorodeckiy88@gmail.com>
2021-11-01 20:56:51 +0200

    added HW10

---
+++ HW10/index.js
@@ -0,0 +1,17 @@
+const container = document.getElementById('box');
+const generateBlocks = () => {
+    for (let i = 0; i < 25; i++) {
+        const div = document.createElement('div')
+        container.append(div)
+        div.style.backgroundColor = "#" + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+    }
+}
+generateBlocks()
+
+const generateBlocksInterval = () => {
+    setInterval(() => {
+        container.innerHTML = ''
+        generateBlocks()
+    }, 1000);
+}
+generateBlocksInterval()
